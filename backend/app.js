69d351be40b116db5c52f4cc0e9819e421240061
vikas dhakad <95852732+dhakadvikas@users.noreply.github.com>
2025-04-30 10:30:11 +0530

    Update cors config

--- backend/app.js
+++ backend/app.js
@@ -55,7 +55,13 @@ const cors = require("cors");
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors(
+  {
+    origin: "https://uitrgpv.netlify.app/",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }
+));
 app.use("/uploads", express.static("uploads"));
 
 
