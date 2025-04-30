cc1aef573b610034b6cd97dc1a41951ad499b0d9
vikas dhakad <95852732+dhakadvikas@users.noreply.github.com>
2025-04-30 11:06:53 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -55,7 +55,7 @@ const cors = require("cors");
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(());
+app.use(cors());
 app.use("/uploads", express.static("uploads"));
 
 
